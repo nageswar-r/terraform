2789cc3158ddc6b9a7474794c08eb62075c1e5e2
Nageswar <nagesh23479@gmail.com>
2026-03-02 10:40:48 +0530

    test

---
+++ feb-26-modules-ec2-test/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "env" {
+    default = "dev"  
+}
+
+variable "sg_ids" {
+    default = ["sg-0a7b91aa77cf9de83"]
+}
+
+variable "component" {
+    default = "catalogue"
+}
